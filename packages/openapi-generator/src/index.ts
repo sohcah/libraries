d12ddc04325af236892ef09ca6309ec4bc10d533
Sam Hindess <mail@sohcah.dev>
2026-01-20 22:29:10 +0000

    Use Parcel Watcher, Fix Binary Support, Retries and Logging for Watch Mode (#10)

--- packages/openapi-generator/src/index.ts
+++ packages/openapi-generator/src/index.ts
@@ -5,7 +5,7 @@ import {
   generate as generateEffect,
   type GeneratorOptions,
 } from "./generator.js";
-import { Effect, Stream } from "effect";
+import { Console, Effect, Stream } from "effect";
 import { FileSystem, type WatchEvent } from "@effect/platform/FileSystem";
 import { upgrade } from "@scalar/openapi-upgrader";
 import { FailedToUpgradeOpenApiDocumentError } from "./errors.js";
@@ -40,31 +40,56 @@ export const generate = Effect.fn(function* ({
   return babelGenerate(program).code;
 });
 
+const time = () => new Date().toLocaleTimeString("en-GB");
+
 export const generateToFile = Effect.fn(function* ({
   onGenerated,
   output,
   watch,
   ...options
 }: OpenapiGenerateToFileOptions) {
+  let lastCode: string | null = null;
+
   const fs = yield* FileSystem;
-  let lastCode = yield* generate(options);
-  yield* fs.writeFileString(output, lastCode);
-  onGenerated?.();
+
+  const generateCode = () =>
+    Effect.catchAll(
+      Effect.gen(function* () {
+        if (watch) yield* Console.info(`[${time()}] Generating code...`);
+        const outputCode = yield* generate(options);
+        if (outputCode !== lastCode) {
+          yield* fs.writeFileString(output, outputCode);
+          onGenerated?.();
+          lastCode = outputCode;
+          if (watch) {
+            yield* Console.info(`[${time()}] Generated code successfully`);
+          }
+        } else if (watch) {
+          yield* Console.info(`[${time()}] Code is already up to date`);
+        }
+      }),
+      Effect.fn(function* (error) {
+        if (watch) {
+          yield* Console.error(`[${time()}] Failed to generate code`, error);
+        } else {
+          yield* Effect.fail(error);
+        }
+      })
+    );
+
+  yield* generateCode();
 
   if (watch) {
     if (typeof options.schema !== "string") {
-      throw new Error("Schema must a file path to use watch mode");
+      return yield* Effect.die(
+        new Error("Schema must be a file path to use watch mode")
+      );
     }
     yield* Stream.runForEach(
       fs.watch(options.schema),
       Effect.fn(function* (event: WatchEvent) {
         if (event._tag === "Update") {
-          const outputCode = yield* generate(options);
-          if (outputCode !== lastCode) {
-            yield* fs.writeFileString(output, outputCode);
-            onGenerated?.();
-            lastCode = outputCode;
-          }
+          yield* generateCode();
         }
       })
     );
